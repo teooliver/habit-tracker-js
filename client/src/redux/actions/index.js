a46a5af92c6c480918efb0b15d724cb9a6033e78
Teo Oliver <teo.liz.oli@gmail.com>
2019-11-14 19:58:07 +0000

    ADD_POINT and REMOVE_POINT types

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -9,3 +9,21 @@ export const getHabits = () => async dispatch => {
     payload: res.data
   });
 };
+
+export const addPoint = () => async dispatch => {
+  const res = await axios.post("http://localhost:3001/habits");
+
+  dispatch({
+    type: GET_HABITS,
+    payload: res.data
+  });
+};
+
+export const removePoint = () => async dispatch => {
+  const res = await axios.delete("http://localhost:3001/habits");
+
+  dispatch({
+    type: GET_HABITS,
+    payload: res.data
+  });
+};
--- client/src/redux/actions/types.js
+++ client/src/redux/actions/types.js
@@ -1,1 +1,3 @@
 export const GET_HABITS = "GET_HABITS";
+export const ADD_POINT = "ADD_POINT";
+export const REMOVE_POINT = "REMOVE_POINT";
